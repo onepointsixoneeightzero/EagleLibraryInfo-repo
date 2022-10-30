f3c0774775a93056d62d40da1dbc8a15a5dc16df
Nabeel ur Rehman Rajput <nblrehman@gmail.com>
2022-10-30 20:19:29 +0500

    symParsepin, symParsetext, symParseshape functions added in SYmbolParse.py which returns pin details, text and shape details respectively

--- SymbolParse.py
+++ SymbolParse.py
@@ -1,9 +1,7 @@
 import xml.etree.ElementTree as ET
 import pandas as pd
 
-symbols_path = "./drawing/library/symbols/"
-sym_start_string = symbols_path + "symbol[@name='"
-sym_end_path = "']/"
+
 
 def Library_root(path_and_file):
 
@@ -12,6 +10,9 @@ def Library_root(path_and_file):
     return root
 
 def Symbol_and_path (root_):
+    symbols_path = "./drawing/library/symbols/"
+    sym_start_string = symbols_path + "symbol[@name='"
+    sym_end_path = "']/"
 
     i=0
     SymNames = []
@@ -24,23 +25,10 @@ def Symbol_and_path (root_):
         i += 1
     return SymAndPathDict
 
-def symParse_pin(root_):
-    i=0
+# def symParse_detail (root_,libPath):
+#
 
-    SymAndPathDict = {}
-    SymNames = []
-    SymPath = []
-
-
-    simpleSYMdetail = {}
-
-    Symbol_Attributes_Detail = {}
-    Sym_Text_x = []
-    Sym_Text_y = []
-    Sym_Text_size = []
-    Sym_Text_layer = []
-    Sym_Text_actualText = []
-
+def symParse_pin (root_,libPath):
     Symbol_pin_Detail = {}
     Sym_Pin_Name = []
     Sym_Pin_X = []
@@ -49,7 +37,51 @@ def symParse_pin(root_):
     Sym_Pin_Rot = []
     Sym_Pin_Dir = []
     Sym_Pin_Funct = []
+    for item_ in root_.findall(libPath):
+        if item_.tag == 'pin':
+            Sym_Pin_Name.append(item_.attrib.get('name'))
+            Sym_Pin_X.append(item_.attrib.get('x'))
+            Sym_Pin_Y.append(item_.attrib.get('y'))
+            Sym_Pin_Length.append(item_.attrib.get('length'))
+            Sym_Pin_Rot.append(item_.attrib.get('rot'))
+            Sym_Pin_Dir.append(item_.attrib.get('direction'))
+            Sym_Pin_Funct.append(item_.attrib.get('function'))
+    Symbol_pin_Detail['PinName'] = Sym_Pin_Name
+    Symbol_pin_Detail['xLoc'] = Sym_Pin_X
+    Symbol_pin_Detail['yLoc'] = Sym_Pin_Y
+    Symbol_pin_Detail['PinLength'] = Sym_Pin_Length
+    Symbol_pin_Detail['Rot'] = Sym_Pin_Rot
+    Symbol_pin_Detail['PinDir'] = Sym_Pin_Dir
+    Symbol_pin_Detail['PinFunc'] = Sym_Pin_Funct
 
+    symPindata = pd.DataFrame(data=Symbol_pin_Detail)
+    #print(symPindata)
+    return symPindata
+
+def symParse_text (root_,libPath):
+    Symbol_Attributes_Detail = {}
+    Sym_Text_x = []
+    Sym_Text_y = []
+    Sym_Text_size = []
+    Sym_Text_layer = []
+    Sym_Text_actualText = []
+    for item_ in root_.findall(libPath):
+        if item_.tag == 'text':
+            Sym_Text_x.append(item_.attrib.get('x'))
+            Sym_Text_y.append(item_.attrib.get('y'))
+            Sym_Text_size.append(item_.attrib.get('size'))
+            Sym_Text_layer.append(item_.attrib.get('layer'))
+            Sym_Text_actualText.append(item_.text)
+    Symbol_Attributes_Detail['x'] = Sym_Text_x
+    Symbol_Attributes_Detail['y'] = Sym_Text_y
+    Symbol_Attributes_Detail['size'] = Sym_Text_size
+    Symbol_Attributes_Detail['layer'] = Sym_Text_layer
+    Symbol_Attributes_Detail['Contains'] = Sym_Text_actualText
+
+    symTextdata = pd.DataFrame(data=Symbol_Attributes_Detail)
+    return symTextdata
+
+def symParse_shape (root_,libPath):
     Symbol_shape_Details = {}
     Sym_Shape_x1 = []
     Sym_Shape_x2 = []
@@ -58,84 +90,20 @@ def symParse_pin(root_):
     Sym_Shape_width = []
     Sym_Shape_layer = []
 
-    for item in root_.findall(symbols_path):
-        SymNames.append(item.attrib.get('name'))
-        string = sym_start_string + SymNames[i] + sym_end_path
-        SymAndPathDict[SymNames[i]]=string
-
-        # Get all symbol names and store in sym_name list
-        for item_ in root_.findall(string):
-            if item_.tag == 'pin':
-                Sym_Pin_Name.append(item_.attrib.get('name'))
-                Sym_Pin_X.append(item_.attrib.get('x'))
-                Sym_Pin_Y.append(item_.attrib.get('y'))
-                Sym_Pin_Length.append(item_.attrib.get('length'))
-                Sym_Pin_Rot.append(item_.attrib.get('rot'))
-                Sym_Pin_Dir.append(item_.attrib.get('direction'))
-                Sym_Pin_Funct.append(item_.attrib.get('function'))
-            if item_.tag == 'wire':
-                Sym_Shape_x1.append(item_.attrib.get('x1'))
-                Sym_Shape_y1.append(item_.attrib.get('y1'))
-                Sym_Shape_x2.append(item_.attrib.get('x2'))
-                Sym_Shape_y2.append(item_.attrib.get('y2'))
-                Sym_Shape_width.append(item_.attrib.get('width'))
-                Sym_Shape_layer.append(item_.attrib.get('layer'))
-            if item_.tag == 'text':
-                Sym_Text_x.append(item_.attrib.get('x'))
-                Sym_Text_y.append(item_.attrib.get('y'))
-                Sym_Text_size.append(item_.attrib.get('size'))
-                Sym_Text_layer.append(item_.attrib.get('layer'))
-                Sym_Text_actualText.append(item_.text)
-
-        Symbol_pin_Detail['PinName'] = Sym_Pin_Name
-        Symbol_pin_Detail['xLoc'] = Sym_Pin_X
-        Symbol_pin_Detail['yLoc'] = Sym_Pin_Y
-        Symbol_pin_Detail['PinLength'] = Sym_Pin_Length
-        Symbol_pin_Detail['Rot'] = Sym_Pin_Rot
-        Symbol_pin_Detail['PinDir'] = Sym_Pin_Dir
-        Symbol_pin_Detail['PinFunc'] = Sym_Pin_Funct
-
+    # Get all symbol names and store in sym_name list
+    for item_ in root_.findall(libPath):
+        if item_.tag == 'wire':
+            Sym_Shape_x1.append(item_.attrib.get('x1'))
+            Sym_Shape_y1.append(item_.attrib.get('y1'))
+            Sym_Shape_x2.append(item_.attrib.get('x2'))
+            Sym_Shape_y2.append(item_.attrib.get('y2'))
+            Sym_Shape_width.append(item_.attrib.get('width'))
+            Sym_Shape_layer.append(item_.attrib.get('layer'))
         Symbol_shape_Details['x1'] = Sym_Shape_x1
         Symbol_shape_Details['y1'] = Sym_Shape_y1
         Symbol_shape_Details['x2'] = Sym_Shape_x2
         Symbol_shape_Details['y2'] = Sym_Shape_y2
         Symbol_shape_Details['width'] = Sym_Shape_width
         Symbol_shape_Details['layer'] = Sym_Shape_layer
-
-        Symbol_Attributes_Detail['x'] = Sym_Text_x
-        Symbol_Attributes_Detail['y'] = Sym_Text_y
-        Symbol_Attributes_Detail['size'] = Sym_Text_size
-        Symbol_Attributes_Detail['layer'] = Sym_Text_layer
-        Symbol_Attributes_Detail['Contains'] = Sym_Text_actualText
-
-        Sym_Pin_Name = []
-        Sym_Pin_X = []
-        Sym_Pin_Y = []
-        Sym_Pin_Length = []
-        Sym_Pin_Rot = []
-        Sym_Pin_Dir = []
-        Sym_Pin_Funct = []
-
-        Sym_Shape_x1 = []
-        Sym_Shape_x2 = []
-        Sym_Shape_y1 = []
-        Sym_Shape_y2 = []
-        Sym_Shape_width = []
-        Sym_Shape_layer = []
-
-        Sym_Text_x = []
-        Sym_Text_y = []
-        Sym_Text_size = []
-        Sym_Text_layer = []
-        Sym_Text_actualText = []
-
-        symPindata = pd.DataFrame(data=Symbol_pin_Detail)
-        symShapedata = pd.DataFrame(data=Symbol_shape_Details)
-        symTextdata = pd.DataFrame(data=Symbol_Attributes_Detail)
-
-        # print(SymNames[i])
-        # print(symPindata)
-        # print(symShapedata)
-        # print(symTextdata)
-        i += 1
-    #print(SymAndPathDict)
+    symShapedata = pd.DataFrame(data=Symbol_shape_Details)
+    return symShapedata
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ library_names = dg.get_multiple_file('Please Select one or more Eagle Libraries'
 # ask the user for excel file result output
 Excel_prmpt = dg.yes_no_prompt('Output Files ?', 'Want to Save Complete results in EXCEL')
 
+symAndpath ={}
+
 if Excel_prmpt == True:
     print("selected yes")
     save_as = dg.saveAs_Excel("Select location where to save")
@@ -14,6 +16,13 @@ else:
 
 for library_name in library_names:
     xmlroot=symPrsr.Library_root(library_name)
-    symPrsr.symParse_pin(xmlroot)
-    print(symPrsr.Symbol_and_path(xmlroot))
+#    symPrsr.symParse_pin(xmlroot)
+    symAndpath = symPrsr.Symbol_and_path(xmlroot)
+    for keys, values in symAndpath.items():
+        print("********************************************************")
+        print(keys)
+        print(values)
+        print(symPrsr.symParse_shape(xmlroot,values))
+        print("########################################################")
 
+    print("00000000000000000000000000000000000000000000000000000000")
